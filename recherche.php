f73643817b87f45a42bbfe7a20576e6ec9d7a6d7
Youssef El hardouzi <yalam385.ya@gmail.com>
2023-01-10 20:28:59 +0100

    change recherche method from JQuery to js vanilla

--- recherche.php
+++ recherche.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,32 +9,52 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <?php include('header.php'); ?>
     <div class="container">
         <?php include('leftUser.php'); ?>
         <div class="main">
             <label for="recherche">Recherche <input type="text" name="recherche" id="recherche"></label>
-            <div class="table-container" ></div>
+            <div class="table-container"></div>
         </div>
     </div>
     <script>
-        document.getElementById('recherche').addEventListener('input', function(){
-        $.ajax({
-            url: 'search.php?n=' + document.getElementById('recherche').value,
-            type: "GET",
-            dataType: "JSON",
-            success: function(data){
-                var table = '<table class="my-table">';
-                for(var i = 0; i < data.length; i++){
-                    table += '<tr><td><img width="200px" height="200px" src = "'+data[i].photo+'" ></td><td>'+data[i].nom+ ' '+data[i].prenom +'</td><td><a href = "sendInvite.php?id='+data[i].idUser+'">Ajouter</a></td></tr>';
-                }
-                table += '</table>';
-                $('.table-container').html(table);
-            }
-        })
-    })
+        document.getElementById('recherche').addEventListener('input', function() {
+            // $.ajax({
+            //     url: 'search.php?n=' + document.getElementById('recherche').value,
+            //     type: "GET",
+            //     dataType: "JSON",
+            //     success: function(data){
+            //         var table = '<table class="my-table">';
+            //         for(var i = 0; i < data.length; i++){
+            //             table += '<tr><td><img width="200px" height="200px" src = "'+data[i].photo+'" ></td><td>'+data[i].nom+ ' '+data[i].prenom +'</td><td><a href = "sendInvite.php?id='+data[i].idUser+'">Ajouter</a></td></tr>';
+            //         }
+            //         table += '</table>';
+            //         $('.table-container').html(table);
+            //     }
+            // })
+            var input = document.getElementById('recherche');
+            input.addEventListener('input', function() {
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', 'search.php?n=' + input.value, true);
+                xhr.responseType = 'json';
+                xhr.onload = function() {
+                    if (xhr.status === 200) {
+                        var data = xhr.response;
+                        var table = '<table class="my-table">';
+                        for (var i = 0; i < data.length; i++) {
+                            table += '<tr><td><img width="200px" height="200px" src = "' + data[i].photo + '" ></td><td>' + data[i].nom + ' ' + data[i].prenom + '</td><td><a href = "sendInvite.php?id=' + data[i].idUser + '">Ajouter</a></td></tr>';
+                        }
+                        table += '</table>';
+                        document.querySelector('.table-container').innerHTML = table;
+                    }
+                };
+                xhr.send();
+            });
 
+        })
     </script>
 </body>
+
 </html>
