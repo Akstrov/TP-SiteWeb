43513f8522bda51a4fb7db7c5fdf03dc848563f4
Youssef El hardouzi <yalam385.ya@gmail.com>
2023-01-11 12:13:29 +0100

    change search so it won't show people who are in invitations table where the user is exp or des

--- search.php
+++ search.php
@@ -1,17 +1,16 @@
 <?php
-    session_start();
-    include('connect.php');
-    $name = $_GET['n'];
-    $query = "SELECT idUser, nom, prenom, photo FROM users
-    WHERE (nom LIKE '$name%' OR prenom LIKE '$name%') AND idUser != ".$_SESSION['id']."
-    AND (idUser NOT IN (SELECT des FROM invitations WHERE exp = ".$_SESSION['id'].") 
-    OR idUser NOT IN (SELECT exp FROM invitations WHERE des =".$_SESSION['id']."))
+session_start();
+include('connect.php');
+$name = $_GET['n'];
+$query = "SELECT idUser, nom, prenom, photo FROM users
+    WHERE (nom LIKE '$name%' OR prenom LIKE '$name%') AND idUser != " . $_SESSION['id'] . "
+    AND (idUser NOT IN (SELECT des FROM invitations WHERE exp = " . $_SESSION['id'] . ") 
+    AND idUser NOT IN (SELECT exp FROM invitations WHERE des =" . $_SESSION['id'] . "))
     ";
-    $result = mysqli_query($conn,$query);
-    $data = array();
-    while ($row = mysqli_fetch_assoc($result)){
-        $data[] = $row;
-    }
-    $json = json_encode($data);
-    echo $json;
-?>
+$result = mysqli_query($conn, $query);
+$data = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
+$json = json_encode($data);
+echo $json;
